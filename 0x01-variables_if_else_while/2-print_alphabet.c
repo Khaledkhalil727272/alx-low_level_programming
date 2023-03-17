39d87c6068ca4a9fbffa2f7968a3a307f1b05143
Khaled <khaledkhalil727272@gmail.com>
2023-03-17 09:04:06 -0700

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-/***/
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	char i = 97;
-	for(i;i<123;i++)
-		printf("%c",i);
+	for (i; i < 123; i++)
+		printf("%c", i);
+	printf("\n");
 	return (0);
 }
