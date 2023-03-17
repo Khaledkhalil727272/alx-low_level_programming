20bc6ae9627c3efdb4436f1d2a17e756cebe3b81
Khaled <khaledkhalil727272@gmail.com>
2023-03-17 16:13:37 -0700

    my commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if ((j == 8) && (i == 9))
+				continue;
+			putchar(' ');
+			for (k = i; k <= 9; k++)
+			{
+				for (m = k; m <= 9; m++)
+				{
+					if (m == k)
+						continue;
+					putchar((k % 10) + '0');
+					putchar((m % 10) + '0');
+					if ((j == 8) && (i == 9))
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
